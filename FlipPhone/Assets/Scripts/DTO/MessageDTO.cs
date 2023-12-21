46a7ccbd85f09b5b029a6e9a19ba25f4c711c02b
kurobei0314 <maikurodei.biseibutu@gmail.com>
2023-12-22 01:37:23 +0900

    [refactor] monobehaivorに対してnewしないようにした

--- FlipPhone/Assets/Scripts/DTO/MessageDTO.cs
+++ FlipPhone/Assets/Scripts/DTO/MessageDTO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MessageDTO : MonoBehaviour
+public class MessageDTO
 {
     private int id;
     public int Id => id;
